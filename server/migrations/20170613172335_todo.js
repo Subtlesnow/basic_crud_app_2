4da662b5d91957fea6adc7457770825eede25b88
Steven VanWoerkom <vanwoerkom.steve@gmail.com>
2017-06-13 17:41:37 -0600

    Completed migration

--- server/migrations/20170613172335_todo.js
+++ server/migrations/20170613172335_todo.js
@@ -1,8 +1,22 @@
+// List of things to add
+// Title - text
+// Priority - integer
+// Description - text
+// Completed - boolean
+// Date - datetime
+
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('todo', (table) => {
+    table.increments();
+    table.text('title').notNullable();
+    table.integer('priority').notNullable();
+    table.text('description');
+    table.boolean('complete').defaultTo(false).notNullable();
+    table.datetime('date').notNullable();
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('todo');
 };
